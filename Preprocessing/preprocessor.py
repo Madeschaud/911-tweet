47d605cd755c84fe18d415b6a6e3ed9a2d9561d8
Lucas Goumard <goumard.lucas@gmail.com>
2024-03-04 16:48:50 +0100

    Preprocessing + drop null + drop duplicates

--- Preprocessing/preprocessor.py
+++ Preprocessing/preprocessor.py
@@ -9,7 +9,15 @@ def preprocessor():
     data['tweet_clean'] = data['tweet_text'].apply(cleaning)
     data = data.dropna()
     data = data.drop_duplicates()
-    data = data.drop
+
+    data['words_per_tweet'] = data['tweet_clean'].apply(nbwords)
+    data = data[~(data['words_per_tweet'] == 0)]
+
     data.to_csv('Data/clean_data.csv', index=True)
 
     print(data)
+
+def nbwords(sentence):
+    if isinstance(sentence, str) :
+        return len(sentence.split())
+    return 0
--- main.py
+++ main.py
@@ -9,11 +9,7 @@ def main():
         preprocessor()
 
     data_cleaned = pandas.read_csv('Data/clean_data.csv')
-    data_cleaned['words_per_tweet'] = data_cleaned['tweet_clean'].apply(nbwords)
+
     print(data_cleaned)
     print(data_cleaned['words_per_tweet'].mean())
 
-def nbwords(sentence):
-    if isinstance(sentence, str) :
-        return len(sentence.split())
-    return 0
