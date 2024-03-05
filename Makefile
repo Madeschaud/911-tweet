def46955d8192d82248bec8a85dc3b76b15304d1
Josephine Bach <josephinebach25@gmail.com>
2024-03-05 11:24:28 +0100

    no language treatment

--- Makefile
+++ Makefile
@@ -4,8 +4,5 @@ run_preprocess:
 run_main:
 	@python -c 'from main import main; main()'
 
-run_modify_language:
-	@python -c 'from Modify_language.modify_language import modify_language; modify_language()'
-
 install_dep:
 	pip install -r requirements.txt
--- main.py
+++ main.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+def hist_word_distrib(action, data_cleaned):
+    # Histogram of Words Distribution
+    if action is True:
+        plt.figure(figsize=(10, 6))
+        sns.histplot(data_cleaned['words_per_tweet'], kde=True)
+        plt.title('Histogram of Words Distribution')
+        # plt.xlim(0,50)
+        plt.xlabel('Values')
+        plt.ylabel('Frequency')
+        plt.show()
+        print('mean = ',data_cleaned['words_per_tweet'].mean())
+        print('min = ',data_cleaned['words_per_tweet'].min())
+        print('max = ', data_cleaned['words_per_tweet'].max())
+
+
 def main():
     print(Fore.RED + 'Main' + Fore.WHITE)
     if not os.path.isfile('Data/clean_data.csv'):
@@ -13,17 +28,8 @@ def main():
 
     data_cleaned = pandas.read_csv('Data/clean_data.csv', index_col=0)
 
-    # Histogram of Words Distribution
-    plt.figure(figsize=(10, 6))
-    sns.histplot(data_cleaned['words_per_tweet'], kde=True)
-    plt.title('Histogram of Words Distribution')
-    # plt.xlim(0,50)
-    plt.xlabel('Values')
-    plt.ylabel('Frequency')
-    plt.show()
+    #Turn true or False to activate or disactivate the histplot
+    hist_word_distrib(False, data_cleaned)
 
-    print(data_cleaned.columns)
-    print(len(data_cleaned))
-    print('mean = ',data_cleaned['words_per_tweet'].mean())
-    print('min = ',data_cleaned['words_per_tweet'].min())
-    print('max = ', data_cleaned['words_per_tweet'].max())
+    # data_cleaned["test_regex"]= data_cleaned["tweet_clean"].str.find('@')
+    # print(data_cleaned['test_regex'].value_counts())
