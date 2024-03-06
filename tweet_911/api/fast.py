7a10b4c3b92639f91d9a4e3d5a5f238b1266b923
Josephine Bach <josephinebach25@gmail.com>
2024-03-06 17:02:37 +0100

    api ok

--- tweet_911/api/fast.py
+++ tweet_911/api/fast.py
@@ -20,7 +20,7 @@ app.add_middleware(
 # The trick is to load the model in memory when the Uvicorn server starts
 # and then store the model in an `app.state.model` global variable, accessible across all routes!
 # This will prove very useful for the Demo Day
-app.state.model = model_bidirectional_lstm()
+# app.state.model = model_bidirectional_lstm()
 
 # http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
 @app.get("/predict")
