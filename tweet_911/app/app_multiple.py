c1e01169ea3ee1c592582569f874a2ad1b940bf5
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 10:48:58 +0100

    application ok

--- tweet_911/app/app_multiple.py
+++ tweet_911/app/app_multiple.py
@@ -33,7 +33,7 @@ st.markdown("""
             border-color: #1DA1F2 !important;
             border-radius:20px  !important;
             padding:10px 20px 10px 20px !important;
-            margin-top:50% !important;
+            # margin-top:30% !important;
         }
         .stButton{
             display:flex;
@@ -53,6 +53,18 @@ st.markdown("""
          .e16nr0p34 p a{
              color:white;
         }
+
+        .css-5rimss p{
+            border:1px solid rgba(49, 51, 63, 0.2);;
+            border-radius:20px;
+            text-align:center;
+            padding:18px;
+            color:#1DA1F2;
+        }
+        .css-5rimss p a{
+            color:#1DA1F2;
+        }
+
 
     </style>
     """, unsafe_allow_html=True)
@@ -91,9 +103,7 @@ def mark_disaster():
         print(pred)
         if round(pred, 2) > 0.3:
             col1.write(st.session_state.display_tweet.iloc[index])
-
-        # else:
-        #     next_tweet()
+    st.session_state.stage = 1
 
 def mark_actionable():
 
@@ -105,24 +115,26 @@ def mark_actionable():
     # else:
     #     next_tweet()
 
-def display_tweet():
-    with col1:
-        # for index in range(0, len(st.session_state.display_tweet)):
+# def display_tweet():
+#     with col1:
+#         # if st.session_state.stage == 1:
+#             print('here')
+#             col1.write(st.session_state.display_tweet)
+#             st.button('Is this tweet actionable?', on_click=mark_actionable)
 
-        if st.session_state.stage == 1:
-            print('here')
-            col1.write(st.session_state.display_tweet)
-            st.button('Is this tweet actionable?', on_click=mark_actionable)
+
+
 col1, col2 = st.columns([1, 1], gap="small")
 
 with st.sidebar:
     if st.session_state.stage == 0:
         for index in range(0, len(st.session_state.display_tweet)):
-            st.write(st.session_state.display_tweet.iloc[index])
+            st.sidebar.write(st.session_state.display_tweet.iloc[index])
 
 with col1:
     st.markdown("<h1 style='text-align: center;'>Disaster<br>Tweet</h1>", unsafe_allow_html=True)
     st.markdown('---')
+    st.button('Find if the tweets are disasters', on_click=mark_disaster, key='button_state')
 
 
 with col2:
@@ -132,4 +144,3 @@ with col2:
 #         st.write(st.session_state.display_tweet)
 #         st.button('I want a new Tweet', on_click=next_tweet)
 # # with footer:
-st.button('Find if the tweets are disasters', on_click=mark_disaster, key='button_state')
