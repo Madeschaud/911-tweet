9069a8b10b5117c8871f46aeff96b58c3f0d0c95
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 14:26:03 +0100

    app modif

--- tweet_911/app/app_multiple.py
+++ tweet_911/app/app_multiple.py
@@ -110,6 +110,7 @@ def mark_disaster():
 
 def mark_actionable():
     print(st.session_state.new_tweets)
+    st.write(f'{URL_API}/predict_actionable', { 'tweet': st.session_state.new_tweets[index] })
     for index in range(0, len(st.session_state.new_tweets)):
         response = requests.get(f'{URL_API}/predict_actionable', { 'tweet': st.session_state.new_tweets[index] })
         prediction = response.json()
