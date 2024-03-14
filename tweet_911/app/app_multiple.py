acc09c1da9e7d304d482c672390fb13bb9f04f00
mathilde deschaud <mathilde.deschaud@hotmail.fr>
2024-03-14 12:55:57 +0100

    app

--- tweet_911/app/app_multiple.py
+++ tweet_911/app/app_multiple.py
@@ -52,10 +52,10 @@ st.markdown("""
             border-radius:20px;
             text-align:center;
             padding:18px;
-            color:#1DA1F2;
+            color:rgba(49, 51, 63, 0.2);
         }
         .e1tzin5v1 .e1tzin5v0  .stMarkdown .e16nr0p34 p a{
-            color:#1DA1F2;
+            color:rgba(49, 51, 63, 0.2);
         }
 
         .css-6qob1r{
