7f2c966a4bf33fca1e0f160fb4228f1d68d89382
Josephine Bach <josephinebach25@gmail.com>
2024-03-14 14:04:40 +0100

    app modif

--- tweet_911/app/app_multiple.py
+++ tweet_911/app/app_multiple.py
@@ -118,7 +118,7 @@ def mark_actionable():
         if round(pred, 2) > 0.3:
             col2.write(st.session_state.new_tweets[index])
     st.session_state.stage = 2
-    st.switch_page("pages/experience.py")
+    # st.switch_page("pages/experience.py")
 
 
 
