61d173afc401351574108d413044409e38737683
mathilde deschaud <mathilde.deschaud@hotmail.fr>
2024-03-14 11:51:57 +0100

    csv

--- tweet_911/app/app_multiple.py
+++ tweet_911/app/app_multiple.py
@@ -83,8 +83,7 @@ if 'display_tweet' not in st.session_state:
 def mark_disaster():
     for index in range(0, len(st.session_state.display_tweet)):
         print(st.secrets['URL_API'])
-        response = requests.get(f'/predict_disaster', { 'tweet': st.session_state.display_tweet.iloc[index] })
-        # response = requests.get(f'{st.secrets['url_api']['URL_API']}/predict_disaster', { 'tweet': st.session_state.display_tweet.iloc[index] })
+        response = requests.get(f'{st.secrets['URL_API']}/predict_disaster', { 'tweet': st.session_state.display_tweet.iloc[index] })
         prediction = response.json()
         pred = prediction['tweet_accurate']
         print(pred)
